4c1672610c419e4b61905ed519541f8e1797b10d
SeongTae Jeong <kevin13@chol.net>
2020-01-07 19:25:28 +0900

    Add project files.

---
+++ KernelMemoryIO/DriverMain.c
@@ -0,0 +1,250 @@
+
+#include <ntddk.h>
+#include <strsafe.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    NTSTATUS MajorDeviceControl(PDEVICE_OBJECT pDeviceObject, PIRP pIrp);
+    NTSTATUS MajorCreate(IN PDEVICE_OBJECT pDeviceObject, IN PIRP pIrp);
+    NTSTATUS MajorClose(IN PDEVICE_OBJECT pDeviceObject, IN PIRP pIrp);
+    VOID DriverUnload(PDRIVER_OBJECT pDriverObject);
+    NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath);
+#ifdef __cplusplus
+}
+#endif
+
+#include "MemoryIO.h"
+
+VOID DriverUnload(PDRIVER_OBJECT pDriverObject)
+{
+    UNICODE_STRING uniDOSString;
+
+    RtlInitUnicodeString(&uniDOSString, DOS_DEVICE_NAME);
+    IoDeleteSymbolicLink(&uniDOSString);
+    IoDeleteDevice(pDriverObject->DeviceObject);
+
+    DbgPrint("KernelMemoryIO Driver Unloaded\n");
+}
+
+// WARNING!!!
+// This driver must not be deployed in your product.
+// Definitely, you shouldn't sign this module except only in case of testing or studying.
+//
+// Allowing user-mode program to read and write memory in kernel space has seriously security vulnerabilities.
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath)
+{
+    UNREFERENCED_PARAMETER(pRegistryPath);
+
+    NTSTATUS ntStatus;
+    PDEVICE_OBJECT DeviceObject = NULL;
+    UNICODE_STRING uniNameString, uniDOSString;
+
+    RtlInitUnicodeString(&uniNameString, DEVICE_NAME);
+    RtlInitUnicodeString(&uniDOSString, DOS_DEVICE_NAME);
+
+    ntStatus = IoCreateDevice(pDriverObject,
+        sizeof(MEMORYIO_DEVICE_EXTENSION),
+        &uniNameString,
+        FILE_DEVICE_UNKNOWN,
+        0,
+        FALSE,
+        &DeviceObject);
+
+    if (NT_SUCCESS(ntStatus) == FALSE)
+    {
+        return ntStatus;
+    }
+
+    PMEMORYIO_DEVICE_EXTENSION deviceExtension = (PMEMORYIO_DEVICE_EXTENSION)DeviceObject->DeviceExtension;
+    deviceExtension->Position = NULL;
+
+    ntStatus = IoCreateSymbolicLink(&uniDOSString, &uniNameString);
+
+    if (NT_SUCCESS(ntStatus) == FALSE)
+    {
+        return ntStatus;
+    }
+
+    pDriverObject->MajorFunction[IRP_MJ_CREATE] = MajorCreate;
+    pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = MajorDeviceControl;
+    pDriverObject->MajorFunction[IRP_MJ_CLOSE] = MajorClose;
+    pDriverObject->DriverUnload = DriverUnload;
+
+    DbgPrint("KernelMemoryIO Driver loaded\n");
+
+    return STATUS_SUCCESS;
+}
+
+PUCHAR BytesToPtr(PVOID ioBuffer)
+{
+    int ptrSize = sizeof(void*);
+    PUCHAR copyPtr = NULL;
+
+    if (ptrSize == 8)
+    {
+        memcpy(&(__int64)copyPtr, ioBuffer, 8);
+        return copyPtr;
+    }
+    else
+    {
+#pragma warning(disable: 4311 4302)
+        memcpy(&(int)copyPtr, ioBuffer, 4);
+#pragma warning(default: 4311 4302)
+        return copyPtr;
+    }
+}
+
+NTSTATUS MajorDeviceControl(PDEVICE_OBJECT pDeviceObject, PIRP pIrp)
+{
+    PIO_STACK_LOCATION  irpSp;
+    NTSTATUS            ntStatus = STATUS_SUCCESS;
+
+    ULONG               inBufLength;   /* Input buffer length */
+    ULONG               outBufLength;  /* Output buffer length */
+    PVOID               ioBuffer;
+
+    ULONG ptrSize = sizeof(void*);
+
+    irpSp = IoGetCurrentIrpStackLocation(pIrp);
+
+    inBufLength = irpSp->Parameters.DeviceIoControl.InputBufferLength;
+    outBufLength = irpSp->Parameters.DeviceIoControl.OutputBufferLength;
+
+    ioBuffer = pIrp->AssociatedIrp.SystemBuffer;
+    pIrp->IoStatus.Information = 0; /* Output Buffer Size */
+
+    PMEMORYIO_DEVICE_EXTENSION deviceExtension = (PMEMORYIO_DEVICE_EXTENSION)pDeviceObject->DeviceExtension;
+
+    switch (irpSp->Parameters.DeviceIoControl.IoControlCode)
+    {
+        case IOCTL_READ_MEMORY:
+            if (inBufLength != ptrSize)
+            {
+                ntStatus = STATUS_BUFFER_TOO_SMALL;
+            }
+            else
+            {
+                PUCHAR ptr = BytesToPtr(ioBuffer);
+                deviceExtension->Position = ptr;
+
+                /*
+                PUCHAR outBuffer = (PUCHAR)ioBuffer;
+                for (ULONG i = 0; i < outBufLength; i++)
+                {
+                    outBuffer[i] = *(ptr + i);
+                }
+                */
+
+                MM_COPY_ADDRESS address;
+                address.VirtualAddress = ptr;
+
+                SIZE_T numberOfBytesTransferred = 0;
+                MmCopyMemory(ioBuffer, address, outBufLength, MM_COPY_MEMORY_VIRTUAL, &numberOfBytesTransferred);
+
+                pIrp->IoStatus.Information = numberOfBytesTransferred;
+                ntStatus = STATUS_SUCCESS;
+            }
+            break;
+
+        case IOCTL_GETPOS_MEMORY:
+            if (outBufLength != ptrSize)
+            {
+                ntStatus = STATUS_BUFFER_TOO_SMALL;
+            }
+            else
+            {
+                MM_COPY_ADDRESS address;
+                address.VirtualAddress = &deviceExtension->Position;
+
+                SIZE_T numberOfBytesTransferred = 0;
+                MmCopyMemory(ioBuffer, address, ptrSize, MM_COPY_MEMORY_VIRTUAL, &numberOfBytesTransferred);
+
+                pIrp->IoStatus.Information = numberOfBytesTransferred;
+                ntStatus = STATUS_SUCCESS;
+            }
+            break;
+
+        case IOCTL_SETPOS_MEMORY:
+            if (inBufLength != ptrSize)
+            {
+                ntStatus = STATUS_BUFFER_TOO_SMALL;
+            }
+            else
+            {
+                PUCHAR ptr = BytesToPtr(ioBuffer);
+                deviceExtension->Position = ptr;
+
+                ntStatus = STATUS_SUCCESS;
+            }
+            break;
+
+        case IOCTL_WRITE_MEMORY:
+            if (inBufLength == 0)
+            {
+                ntStatus = STATUS_BUFFER_TOO_SMALL;
+            }
+            else
+            {
+                PUCHAR ptr = deviceExtension->Position;
+
+                /*
+                PUCHAR inBuffer = (PUCHAR)ioBuffer;
+                for (ULONG i = 0; i < inBufLength; i++)
+                {
+                    *(ptr + i) = inBuffer[i];
+                }
+                */
+
+                MM_COPY_ADDRESS address;
+                address.VirtualAddress = ioBuffer;
+
+                SIZE_T numberOfBytesTransferred = 0;
+                MmCopyMemory(ptr, address, inBufLength, MM_COPY_MEMORY_VIRTUAL, &numberOfBytesTransferred);
+                pIrp->IoStatus.Information = numberOfBytesTransferred;
+
+                ntStatus = STATUS_SUCCESS;
+            }
+            break;
+
+        default:
+            ntStatus = STATUS_UNSUCCESSFUL;
+            break;
+    }
+
+    pIrp->IoStatus.Status = ntStatus;
+    IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+    return ntStatus;
+}
+
+NTSTATUS MajorCreate(IN PDEVICE_OBJECT pDeviceObject, IN PIRP pIrp)
+{
+    UNREFERENCED_PARAMETER(pDeviceObject);
+
+    NTSTATUS ntStatus;
+
+    pIrp->IoStatus.Information = 0;
+    pIrp->IoStatus.Status = STATUS_SUCCESS;
+
+    ntStatus = pIrp->IoStatus.Status;
+    IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS MajorClose(IN PDEVICE_OBJECT pDeviceObject, IN PIRP pIrp)
+{
+    UNREFERENCED_PARAMETER(pDeviceObject);
+
+    NTSTATUS ntStatus;
+
+    pIrp->IoStatus.Status = STATUS_SUCCESS;
+    pIrp->IoStatus.Information = 0;
+
+    ntStatus = pIrp->IoStatus.Status;
+    IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+    return ntStatus;
+}
---
+++ MemoryIOSample/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.Threading;
+using System.Media;
+
+namespace MemoryIOLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Version 1.0");
+
+            using (MemoryIOLib memoryIO = new MemoryIOLib())
+            {
+                if (memoryIO.IsInitialized == false)
+                {
+                    Console.WriteLine("Failed to open device");
+                    return;
+                }
+
+                // 0xFFFF878274668080 from process explorer (Handles view pane: Ctrl+H)
+                // It must be a Handle of Thead type.
+                IntPtr ethreadPtr = new IntPtr(unchecked((long)0xFFFF878274668080));
+
+                {
+                    IntPtr clientIdPtr = ethreadPtr + 0x648;
+                    byte[] buffer = new byte[16];
+
+                    if (memoryIO.ReadMemory(clientIdPtr, buffer) != buffer.Length)
+                    {
+                        Console.WriteLine("failed to read");
+                        return;
+                    }
+
+                    long value = BitConverter.ToInt64(buffer, 0);
+                    Console.WriteLine("PID: " + value + "(" + value.ToString("x") + ")");
+                    value = BitConverter.ToInt64(buffer, 8);
+                    Console.WriteLine("TID: " + value + "(" + value.ToString("x") + ")");
+                }
+
+                {
+                    //    +0x220 Process          : Ptr64 _KPROCESS
+                    IntPtr kprocessPosPtr = ethreadPtr + 0x220;
+                    byte[] buffer = new byte[8];
+
+                    if (memoryIO.ReadMemory(kprocessPosPtr, buffer) != buffer.Length)
+                    {
+                        Console.WriteLine("failed to read");
+                        return;
+                    }
+
+                    IntPtr kprocessPtr = new IntPtr(BitConverter.ToInt64(buffer, 0));
+                    Console.WriteLine("_EPROCESS: " + kprocessPtr + "(" + kprocessPtr.ToString("x") + ")");
+
+                    {
+                        IntPtr cookiePtr = kprocessPtr + 0x3d0;
+
+                        buffer = new byte[4];
+
+                        if (memoryIO.ReadMemory(cookiePtr, buffer) != buffer.Length)
+                        {
+                            Console.WriteLine("failed to read");
+                            return;
+                        }
+
+                        int oldCookie = BitConverter.ToInt32(buffer, 0);
+                        Console.WriteLine("[OLD] _EPROCESS.cookie: " + oldCookie + "(" + oldCookie.ToString("x") + ")");
+
+                        int writtenBytes = memoryIO.WriteMemory(cookiePtr, BitConverter.GetBytes(0x5000));
+                        Console.WriteLine("Written = " + writtenBytes);
+
+                        memoryIO.ReadMemory(cookiePtr, buffer);
+                        int newCookie = BitConverter.ToInt32(buffer, 0);
+                        Console.WriteLine("[NEW] _EPROCESS.cookie: " + newCookie + "(" + newCookie.ToString("x") + ")");
+
+                        memoryIO.WriteMemory(cookiePtr, BitConverter.GetBytes(oldCookie));
+                        Console.WriteLine("Written = " + writtenBytes);
+                    }
+                }
+
+                //Console.WriteLine($"Current Position: {memoryIO.Position}(" + memoryIO.Position.ToString("x") + ")");
+
+                //memoryIO.Position = ethreadPtr;
+                //Console.WriteLine($"Current Position: {memoryIO.Position}(" + memoryIO.Position.ToString("x") + ")");
+            }
+        }
+    }
+}
